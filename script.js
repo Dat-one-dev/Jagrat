688b2ffea6c0eef65a2ccbd9722636364631b7b3
Dat-one-dev <devselfish@gmail.com>
2026-08-16 21:57:35 +0530

    clocl

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ search.addEventListener("keydown", function (event) {
     }
   }
 });
+
+function samay() {
+  const now = new Date();
+  let hours = now.getHours();
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const ampm = hours >= 12 ? "PM" : "AM"
+
+  hours = hours % 12
+  if (hours === 0) {
+    hours = 12
+  }
+
+  document.getElementById("clock").textContent = `${hours}:${minutes}`
+  document.getElementById("ampm").textContent = ampm;
+  document.getElementById("date").textContent =
+      now.toLocaleDateString("en-IN", {
+          weekday: "long",
+          day: "numeric",
+          month: "long"
+      });
+}
+
+samay();
+setInterval(samay, 1000);
